158191803637f66b9841076fc6ac9d58d50cac35
IvanLin <lyfne@me.com>
2017-02-18 10:37:19 +0800

    Initial Commit

---
+++ AmazingLifeStory/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  AmazingLifeStory
+//
+//  Created by 林逸凡 on 2017/2/18.
+//  Copyright © 2017年 Ivan.lin. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
